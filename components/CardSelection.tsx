71841861b323ac3db76160604b5fc086087bad45
Amogh J <amoghjahagirdargsf@gmail.com>
2019-06-20 00:05:27 +0400

    Fan out animation implemented

---
+++ components/CardSelection.tsx
@@ -0,0 +1,88 @@
+import React from "react";
+import { View, Text, StyleSheet, SafeAreaView } from "react-native";
+import { RectButton } from "react-native-gesture-handler";
+import Animated, {Easing} from "react-native-reanimated";
+import Card, { Card as CardModel, CARD_WIDTH, CARD_HEIGHT } from "./Card";
+import CheckIcon from "./CheckIcon";
+import Thumbnail from "./Thumbnail";
+import {runTiming, bInterpolate} from "react-native-redash";
+interface CardSelectionProps {
+  cards: [CardModel, CardModel, CardModel];
+}
+const { Clock,block,cond,eq,Value, useCode,multiply,concat,set } = Animated;
+const INITIAL_INDEX: number = -1;
+const timing = (animation: Animated.Value<number>, clock: Animated.Clock) =>
+  set(animation, runTiming(clock, 0, { toValue:1, duration: 400, easing : Easing.linear }));
+
+export default ({ cards }: CardSelectionProps) => {
+  const selectedCard = new Value(INITIAL_INDEX);
+  const cardRotation = cards.map(() => new Value(0));
+  const clock = new Clock();
+  const animation = new Value(0);
+  const translateX = new Value(CARD_WIDTH); 
+  useCode(
+    block([cond(eq(selectedCard, INITIAL_INDEX), [
+      timing(animation, clock)]),
+      set(cardRotation[2],bInterpolate(animation,0,-15)),
+      set(cardRotation[0],bInterpolate(animation,0,15))
+    ]),
+    [cards]
+  );
+  return (
+    <View style={styles.container}>
+    <View style={styles.cards}>
+        {cards.map((card, index) => {
+          const rotateZ = concat(cardRotation[index],"deg")
+          return (
+            <Animated.View
+              key={card.id}
+              style={{
+                ...StyleSheet.absoluteFillObject,
+                transform : [
+                  { translateX:multiply(translateX,-1)},
+                  {rotateZ},
+                  {translateX }
+                ]
+              }}
+            >
+              <Card key={card.id} {...{ card }} />
+            
+            </Animated.View>
+          );
+        })}
+      </View>
+      <SafeAreaView>
+        {cards.map(({ id, name, color, thumbnail }, index) => (
+          <RectButton key={id} onPress={() => selectCard(index)}>
+            <View style={styles.button} accessible>
+              <Thumbnail {...{ thumbnail }} />
+              <View style={styles.label}>
+                <Text>{name}</Text>
+              </View>
+              <CheckIcon {...{ color }} />
+            </View>
+          </RectButton>
+        ))}
+      </SafeAreaView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  cards: {
+    flex: 1,
+    backgroundColor: "#f4f6f3"
+  },
+  button: {
+    flexDirection: "row"
+  },
+  label: {
+    flex: 1,
+    borderBottomWidth: 1,
+    borderColor: "#f4f6f3",
+    justifyContent: "center"
+  }
+});
