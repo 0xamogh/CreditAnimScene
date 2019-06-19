d85bd5ba4095c70c95c6ea6c14bcf7510f38da52
Amogh J <amoghjahagirdargsf@gmail.com>
2019-06-20 03:09:17 +0400

    Animation Implementation Complete ✓✓✓

--- components/CardSelection.tsx
+++ components/CardSelection.tsx
@@ -5,11 +5,12 @@ import Animated, {Easing} from "react-native-reanimated";
 import Card, { Card as CardModel, CARD_WIDTH, CARD_HEIGHT } from "./Card";
 import CheckIcon from "./CheckIcon";
 import Thumbnail from "./Thumbnail";
-import {runTiming, bInterpolate} from "react-native-redash";
+import {runTiming, bInterpolate,max} from "react-native-redash";
+//import { add } from "lodash";
 interface CardSelectionProps {
   cards: [CardModel, CardModel, CardModel];
 }
-const { Clock,block,cond,eq,Value, useCode,multiply,concat,set,not,and,clockRunning,neq } = Animated;
+const { Clock,interpolate,block,cond,eq,add,Value,greaterOrEq, useCode,multiply,concat,set,not,and,clockRunning,neq } = Animated;
 const INITIAL_INDEX: number = -1;
 const timing = (animation: Animated.Value<number>, clock: Animated.Clock) =>
   set(animation, runTiming(clock, 0, { toValue:1, duration: 400, easing : Easing.linear }));
@@ -17,11 +18,14 @@ const timing = (animation: Animated.Value<number>, clock: Animated.Clock) =>
 export default ({ cards }: CardSelectionProps) => {
   const selectedCard = new Value(INITIAL_INDEX);
   const cardRotation = cards.map(() => new Value(0));
+  const cardZind  = cards.map(() => new Value(0));
   const clock = new Clock();
   const animation = new Value(0);
   const translateX = new Value(CARD_WIDTH);
+  const cardTranslations  = cards.map(() => new Value(0));
   const isGroupingANimationDone = new Value(0);
   const selectCard = (index: number)=> selectedCard.setValue(index);
+  const shouldUpdateZ =new Value(1);
   useCode(
     block([
       cond(eq(selectedCard, INITIAL_INDEX), [
@@ -47,7 +51,34 @@ export default ({ cards }: CardSelectionProps) => {
         ),
       cond(not(clockRunning(clock)),set(isGroupingANimationDone,1))
       ]
-    )
+    ),
+    ...cards.map((card,index) => cond(
+      and(eq(selectedCard,index),isGroupingANimationDone),
+      [
+          timing(animation, clock),
+          ...cards.map((_card,i) => i)
+          .filter((_card,i) => index !== i)
+          .map((absoluteIndex,i) =>
+          set(
+            cardRotation[absoluteIndex],
+            bInterpolate(animation,cardRotation[absoluteIndex], 7.5*(i % 2 === 0 ? -1: 1)))),
+          set(cardRotation[index],interpolate(animation,{
+            inputRange:[0,0.5,1],
+            outputRange:[0,45,0]
+          })),
+          set(cardTranslations[index],interpolate(animation,{
+            inputRange : [0,0.5,1],
+            outputRange : [0, -CARD_HEIGHT*1.5,1]
+          })),
+          cond(and(greaterOrEq(animation,0.5),shouldUpdateZ), [
+            set(cardZind[index], add(max(...cardZind),1)),
+            set(shouldUpdateZ,0)
+
+          ]),
+          cond(not(clockRunning(clock)),set(shouldUpdateZ,1))
+
+      ]
+    ))
 
   ]),
     [cards]
@@ -56,16 +87,22 @@ export default ({ cards }: CardSelectionProps) => {
     <View style={styles.container}>
     <View style={styles.cards}>
         {cards.map((card, index) => {
-          const rotateZ = concat(cardRotation[index],"deg")
+          const translateY = cardTranslations[index];
+          const elevation = cardZind[index];
+          const zIndex = elevation;
+          const rotateZ = concat(cardRotation[index],"deg");
           return (
             <Animated.View
               key={card.id}
               style={{
                 ...StyleSheet.absoluteFillObject,
+                zIndex,
+                elevation,
                 transform : [
                   { translateX:multiply(translateX,-1)},
                   {rotateZ},
-                  {translateX }
+                  {translateX },
+                  {translateY}
                 ]
               }}
             >
